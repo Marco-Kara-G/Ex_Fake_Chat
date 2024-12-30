21af61eadd74be4215ecab3ed8efe6ecd5d37caf
Marco Grassi <marco.grassi.private@gmail.com>
2024-12-30 13:24:08 +0100

    Exercise about a fake chat with funcional sending button

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ChatComponents } from "./components/chatComponents";
+
+function App() {
+  return (
+    <div>
+      <ChatComponents />
+    </div>
+  );
+}
+
+export default App;
